33983e179699ba6acaa6136bef38fd77e7e5b8c6
nasim <nasimniaziann@gmail.com>
2024-03-06 11:37:22 +0330

    Refactor codes and handle errors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const BASE_URL = "https://api.openweathermap.org/data/2.5";
-const API_KEY = "d198e30f978aa873ac5646f5613330ab";
+import getWeatherData from "./utils/httpReq.js";
+
 const DAYS = [
   "Sunday",
   "Monday",
@@ -15,34 +15,6 @@ const searchButton = document.querySelector("button");
 const weatherContainer = document.getElementById("weather");
 const forecastContainer = document.getElementById("forecast");
 const locationIcon = document.getElementById("location");
-
-const getCurrentWeatherByName = async (city) => {
-  const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
-
-const getCurrentWeatherByCoordinates = async (lat, lon) => {
-  const url = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
-
-const getForecastWeatherByName = async (city) => {
-  const url = `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
-
-const getForecastWeatherByCoordinates = async (lat, lon) => {
-  const url = `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
 
 const renderCurrentWeather = (data) => {
   const weatherJSX = `
@@ -91,20 +63,17 @@ const searchHandler = async () => {
   if (!cityName) {
     alert("Please enter city name!");
   }
-  const currentData = await getCurrentWeatherByName(cityName);
+
+  const currentData = await getWeatherData("current" ,cityName);
   renderCurrentWeather(currentData);
-  const forecastData = await getForecastWeatherByName(cityName);
+  const forecastData = await getWeatherData("forecast" ,cityName);
   renderForecastWeather(forecastData);
 };
 
 const positionCallback = async (position) => {
-  const { latitude, longitude } = position.coords;
-  const currentData = await getCurrentWeatherByCoordinates(latitude, longitude);
+  const currentData = await getWeatherData("current" ,position.coords);
   renderCurrentWeather(currentData);
-  const forecastData = await getForecastWeatherByCoordinates(
-    latitude,
-    longitude
-  );
+  const forecastData = await getWeatherData("forecast" ,position.coords);
   renderForecastWeather(forecastData);
 };
 
---
+++ utils/httpReq.js
@@ -0,0 +1,41 @@
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+const API_KEY = "d198e30f978aa873ac5646f5613330ab";
+
+const getWeatherData = async (type, data) => {
+  let url = null;
+
+  switch (type) {
+    case "current":
+      if (typeof data === "string") {
+        url = `${BASE_URL}/weather?q=${data}&appid=${API_KEY}&units=metric`;
+      } else {
+        url = `${BASE_URL}/weather?lat=${data.latitude}&lon=${data.longitude}&appid=${API_KEY}&units=metric`;
+      }
+      break;
+    case "forecast":
+      if (typeof data === "string") {
+        url = `${BASE_URL}/forecast?q=${data}&appid=${API_KEY}&units=metric`;
+      } else {
+        url = `${BASE_URL}/forecast?lat=${data.latitude}&lon=${data.longitude}&appid=${API_KEY}&units=metric`;
+      }
+      break;
+
+    default:
+      `${BASE_URL}/weather?q=tehran&appid=${API_KEY}&units=metric`;
+      break;
+  }
+
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    if (+json.cod === 200) {
+      return json;
+    } else {
+        console.log(json.message);
+    }
+  } catch (error) {
+    console.log("An error occured when feching data.");
+  }
+};
+
+export default getWeatherData;
